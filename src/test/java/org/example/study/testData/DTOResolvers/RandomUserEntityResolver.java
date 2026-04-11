1d30ac82adbedeb5a8eb2d0b12c7deb14ecd04ca
volodymyrprydatko <uarealtek1994@gmail.com>
2026-04-11 15:16:49 +0300

    working

--- src/test/java/org/example/study/testData/DTOResolvers/RandomUserEntityResolver.java
+++ src/test/java/org/example/study/testData/DTOResolvers/RandomUserEntityResolver.java
@@ -1,26 +1,53 @@
 package org.example.study.testData.DTOResolvers;
 
 import org.example.study.Annotations.RandomUserEntity;
+import org.example.study.Annotations.RandomUserEntityList;
 import org.example.study.DTOs.Entities.UserEntity;
+import org.example.study.StrategyEngine.interfaces.PageGenerationStrategy;
+import org.example.study.testData.BaseParameterResolver;
 import org.jspecify.annotations.NonNull;
 import org.jspecify.annotations.Nullable;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.api.extension.ParameterContext;
 import org.junit.jupiter.api.extension.ParameterResolutionException;
-import org.junit.jupiter.api.extension.ParameterResolver;
+
+import java.util.List;
 
 import static org.example.study.testData.TestData.getSingleValidEntity;
 
 //TODO: look into it
-public class RandomUserEntityResolver implements ParameterResolver {
+public class RandomUserEntityResolver extends BaseParameterResolver {
 
     @Override
-    public boolean supportsParameter(ParameterContext parameterContext, @NonNull ExtensionContext extensionContext) throws ParameterResolutionException {
-        return parameterContext.isAnnotated(RandomUserEntity.class) && parameterContext.getParameter().getType().equals(UserEntity.class);
+    public boolean supportsParameter(@NonNull ParameterContext parameterContext, @NonNull ExtensionContext extensionContext) throws ParameterResolutionException {
+
+        boolean isEligibleForSingle = isAnnotatedWith(parameterContext, RandomUserEntity.class) &&
+                isSuperOf(UserEntity.class, parameterContext);
+        boolean isEligibleForList = isAnnotatedWith(parameterContext, RandomUserEntityList.class) &&
+                isSuperOf(List.class, parameterContext) &&
+                hasParametrizedType(parameterContext) &&
+                isParametrizedTypeOf(UserEntity.class, parameterContext);
+
+        return isEligibleForList || isEligibleForSingle;
     }
 
     @Override
-    public @Nullable UserEntity resolveParameter(@NonNull ParameterContext parameterContext, @NonNull ExtensionContext extensionContext) throws ParameterResolutionException {
-        return getSingleValidEntity();
+    public @Nullable Object resolveParameter(@NonNull ParameterContext parameterContext, @NonNull ExtensionContext extensionContext) throws ParameterResolutionException {
+        if (isAnnotatedWith(parameterContext, RandomUserEntity.class)) {
+            return getSingleValidEntity();
+        } else if (isAnnotatedWith(parameterContext, RandomUserEntityList.class)) {
+            RandomUserEntityList annotation = parameterContext.findAnnotation(RandomUserEntityList.class).
+                    orElseThrow(() -> new ParameterResolutionException("Missing @RandomUserEntityList annotation"));
+            int count = annotation.count();
+            PageGenerationStrategy strategy = pageStrategyMap.get(annotation.strategy());
+            validateStrategyType(annotation.strategy(), count);
+
+            return strategy.generate(UserEntity.class, count);
+        }
+
+        throw new ParameterResolutionException(
+                "Unsupported parameter type or missing annotation for parameter: " +
+                        parameterContext.getParameter().getName()
+        );
     }
 }
