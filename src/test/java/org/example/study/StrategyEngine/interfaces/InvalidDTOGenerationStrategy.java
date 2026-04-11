75f43a01bfbe62cdb9890f094a5d193c8e79bde7
volodymyrprydatko <uarealtek1994@gmail.com>
2026-04-11 09:58:02 +0300

    working

--- src/test/java/org/example/study/StrategyEngine/interfaces/InvalidDTOGenerationStrategy.java
+++ src/test/java/org/example/study/StrategyEngine/interfaces/InvalidDTOGenerationStrategy.java
@@ -5,9 +5,34 @@ import java.lang.reflect.Field;
 import java.util.List;
 
 /**
- * General Idea - invalidate a SPECIFIC field in the DTO judging by the Constraint annotation that is set upon that field
- */
+ * Generic strategy interface for generating invalid DTOs.
+ * Implementations can create invalid instances by violating specific constraints on fields.
+ * */
 public interface InvalidDTOGenerationStrategy {
+
+    /**
+     * Generates a single invalid DTO instance by violating a specific constraint annotation on a field.
+     * This method is used to create an object that fails validation for testing purposes.
+     *
+     * @param clazz the target class to instantiate and modify
+     * @param field the field on which the constraint will be violated
+     * @param annotationToBreak the validation annotation class to break (e.g., Min.class, NotNull.class, Pattern.class)
+     * @return an instance of the specified class with an invalid field value
+     * @throws NoSuchFieldException if the field cannot be found on the class
+     * @throws IllegalAccessException if the field cannot be accessed for modification
+     */
     Object generate(Class<?> clazz, Field field, Class<? extends Annotation> annotationToBreak) throws NoSuchFieldException, IllegalAccessException;
+
+    /**
+     * Generates a list of invalid DTO instances.
+     * This method creates multiple objects that fail validation for testing purposes.
+     * The exact behavior depends on the implementation strategy (e.g., random values, empty values, boundary values).
+     *
+     * @param clazz the target class to instantiate and modify
+     * @param count the number of invalid instances to generate
+     * @return a list containing {@code count} invalid instances of the specified class
+     * @throws NoSuchFieldException if required fields cannot be found on the class
+     * @throws IllegalAccessException if fields cannot be accessed for modification
+     */
     List<?> generateList(Class<?> clazz, int count) throws NoSuchFieldException, IllegalAccessException;
 }
