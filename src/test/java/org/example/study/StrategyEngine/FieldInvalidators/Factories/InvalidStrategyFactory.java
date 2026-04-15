01ec58e08f7dc0a5a35b57b1c8bece9199a78ba1
PrydatkoVolodymyr <uarealtek1994@gmail.com>
2026-04-15 08:44:15 +0300

    working

--- src/test/java/org/example/study/StrategyEngine/FieldInvalidators/Factories/InvalidStrategyFactory.java
+++ src/test/java/org/example/study/StrategyEngine/FieldInvalidators/Factories/InvalidStrategyFactory.java
@@ -11,7 +11,11 @@ import java.util.Map;
 
 /**
  * Object generation dispatcher which produces currently available generation strategies
+ * NOT really a factory though. Just a centralized tool to access a map that contains currently available strategies
  */
+
+//TODO: look into it. Why it is tied up to INVALID data generation. Each strategy could be both
+// if not - we might add more flags / create more strategies so factory can correctly produce desired result
 public class InvalidStrategyFactory {
 
     Map<PageStrategyType, InvalidDTOGenerationStrategy> strategies;
